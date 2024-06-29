0c02ee36378b4547e02088869eca335dc2233662
Rohit Dubey <56528673+rohitdubey1352@users.noreply.github.com>
2024-06-29 19:51:33 +0530

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -14,7 +14,6 @@ import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
